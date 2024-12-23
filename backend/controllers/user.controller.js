7b282594774b809f2fdac7547ce3b36836e95cbe
Sayantansen123 <riosayan123@gmail.com>
2024-12-24 02:27:23 +0530

    logout and login created

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -50,7 +50,7 @@ const signup = async (req, res) => {
         res.status(500).json({ message: 'Failed to send OTP', error });
     }
 
-}
+};
 
 const verifyOtp = async (req, res) => {
     try {
@@ -104,4 +104,43 @@ const verifyOtp = async (req, res) => {
 };
 
 
-export { signup, verifyOtp }
+const login = async (req, res) => {
+    try {
+        const { email, password } = req.body;
+        const user = await User.findOne({ email });
+        console.log(user)
+        const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+        console.log(isPasswordCorrect)
+
+        if (!user || !isPasswordCorrect) return res.status(400).json({ error: "Invalid username or password" });
+
+        generateJWT(user._id, res);
+
+        res.status(200).json({
+            message:"successfully logged in",
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            username: user.username,
+            bio: user.bio,
+            profilePic: user.profilePic,
+        });
+    } catch (err) {
+        console.error("Error in login", err.message);
+        return res.status(500).json({ error: "An error occurred during signup. Please try again." });
+    }
+
+}
+
+const logout = async (req, res) => {
+    try {
+        res.cookie("jwt", "", { maxAge: 1 });
+        res.status(200).json({ message: "User logged out successfully" });
+    } catch (err) {
+        res.status(500).json({ error: err.message });
+        console.log("Error in signupUser: ", err.message);
+    }
+}
+
+
+export { signup, verifyOtp,login,logout }
--- backend/route/user.route.js
+++ backend/route/user.route.js
@@ -1,10 +1,12 @@
 import express from "express"
-import { signup, verifyOtp } from "../controllers/user.controller.js";
+import { login, logout, signup, verifyOtp } from "../controllers/user.controller.js";
 
 const router = express.Router()
 
 router.post("/signup",signup)
 router.post("/verify-otp",verifyOtp)
+router.post("/login",login)
+router.post("/logout",logout)
 
 export default router;
 
